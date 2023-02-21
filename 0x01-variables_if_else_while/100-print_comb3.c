a40546420154152a91861e9043ef945323697527
prisciille <priscille077@gmail.com>
2023-02-20 18:44:39 -0800

    print digit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 48; n <= 57; n++)
+	{
+		for (m = 49; m <= 57; m++)
+		{
+			if (n != m && n < m)
+			{
+			putchar(n);
+			putchar(m);
+			if(n == 56 && m == 57)
+			{
+				break;
+			}
+			putchar(',');
+			putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
